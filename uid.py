b0ba36e17dba47de979bb45a563a6eb5e1610722
Ananya Agrawal <92045291+anagraw@users.noreply.github.com>
2024-06-16 13:06:49 +0530

    Update uid.py

--- uid.py
+++ uid.py
@@ -1,11 +1,32 @@
 import firebase_admin
 from firebase_admin import credentials, auth
+from dotenv import load_dotenv
+import os
 
-# Initialize the Firebase Admin SDK
-cred = credentials.Certificate('serviceacc_key.json')
+# Load environment variables from the .env file
+load_dotenv()
+
+# Use environment variables
+service_account_info = {
+    "type": os.getenv('type'),
+    "project_id": os.getenv('project_id'),
+    "private_key_id": os.getenv('private_key_id'),
+    "private_key": os.getenv('private_key').replace('\\n', '\n'),
+    "client_email": os.getenv('client_email'),
+    "client_id": os.getenv('client_id'),
+    "auth_uri": os.getenv('auth_uri'),
+    "token_uri": os.getenv('token_uri'),
+    "auth_provider_x509_cert_url": os.getenv('auth_provider_x509_cert_url'),
+    "client_x509_cert_url": os.getenv('client_x509_cert_url'),
+    "universe_domain": os.getenv('universe_domain')
+}
+
+# Initialize the Firebase Admin SDK with the environment variables
+cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred, {
-    'projectId': 'ms-hack-e0b6b',
+    'projectId': os.getenv('project_id'),
 })
+
 # Get the most recently signed-in user
 def get_latest_signed_in_user():
     try:
@@ -17,7 +38,7 @@ def get_latest_signed_in_user():
             if not latest_user or user.user_metadata.last_sign_in_timestamp > latest_user.user_metadata.last_sign_in_timestamp:
                 latest_user = user
 
-        if latest_user:
+        if (latest_user):
             print('Most recently signed-in user UID:', latest_user.uid)
         else:
             print('No users found.')
