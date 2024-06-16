b7d54a2bf59b76658247107f8ae0d223a5940590
Saicharan-2004 <117593746+Saicharan-2004@users.noreply.github.com>
2024-06-16 11:04:17 +0530

    Update firebase_admin_config.py

--- firebase_admin_config.py
+++ firebase_admin_config.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Use a service account
-cred = credentials.Certificate('/Users/masterpixel/Documents/Microsoft_Hackathon-main/server/serviceacc_key.json')
+cred = credentials.Certificate('./serviceacc_key.json')
 firebase_admin.initialize_app(cred, {
     'projectId': 'ms-hack-e0b6b',
 })
